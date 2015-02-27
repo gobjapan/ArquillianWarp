dcf42ddaa2abeb77ba6c85d1cb230fde0b187145
gobjapan <ryoko.takida@gmail.com>
2015-02-27 18:23:36 +0900

    kikutaroさんのコードに試しコード追加

--- src/test/java/test/RedirectTest.java
+++ src/test/java/test/RedirectTest.java
@@ -3,12 +3,15 @@ package test;
 import com.mycompany.arquillianwarp.IndexBean;
 import java.io.File;
 import java.net.URL;
+import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.extension.rest.warp.api.RestContext;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.warp.Activity;
 import org.jboss.arquillian.warp.Inspection;
 import org.jboss.arquillian.warp.Warp;
 import org.jboss.arquillian.warp.WarpTest;
@@ -24,6 +27,11 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import static org.jboss.arquillian.warp.client.filter.http.HttpFilters.request;
+import org.jboss.arquillian.warp.client.filter.http.HttpMethod;
+import org.jboss.arquillian.warp.client.result.WarpResult;
+import org.jboss.arquillian.warp.servlet.AfterServlet;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  *
@@ -39,7 +47,7 @@ public class RedirectTest {
 
     @ArquillianResource
     URL contextPath;
-
+    
     @Deployment
     public static WebArchive createDeployment() {
 
@@ -80,5 +88,61 @@ public class RedirectTest {
                 .execute();
         assertThat(browser.getTitle(), is("second"));
     }
-    
+
+    //@Ignore
+    @Test
+    //@RunAsClient
+    public void hello2() {
+        //https://github.com/lfryc/arquillian.github.com/blob/warp-docs/docs/warp.adoc
+        browser.navigate().to(contextPath + "faces/index.xhtml");
+        WarpResult result = Warp.initiate(() -> browser.findElement(By.name("frm:btn")).click())
+                .group("faces/index.xhtml")
+                    .observe(request().uri().contains("index.xhtml"))
+                    .inspect()
+                .group("faces/second.xhtml")
+                    .observe(request().uri().contains("second.xhtml"))//.observe(request().index(2))
+                    .inspect()
+                .execute();
+        assertThat(browser.getTitle(), is("second"));
+    }
+
+    @Test
+    public void hello3() {
+        //画面遷移なら下記でも可能
+        browser.get(contextPath.toExternalForm() + "faces/index.xhtml");
+        browser.findElement(By.name("frm:btn")).click();
+        assertThat(browser.getTitle(), is("second"));
+        
+        //ページに指定文字列が含まれるか
+        assertTrue(browser.getPageSource().contains("Hello from Facelets"));
+    }
+
+    @Test
+    public void shouldMakeProperRequest() {
+        //http://slidedeck.io/mmatloka/arq-extensions-presentation
+        //Arquillian Warp
+        Warp.initiate(new Activity() {
+          @Override
+          public void perform() {
+             browser.navigate().to(contextPath + "faces/index.xhtml");
+          }
+       }).group().observe(request().uri().endsWith("/index.xhtml"))
+       .inspect(new Inspection() {
+            private static final long serialVersionUID = 1L;
+
+            @ArquillianResource
+            private RestContext restContext;
+
+            @AfterServlet
+            public void testGetStocks() {
+                assertTrue(restContext.getHttpResponse().getStatusCode() == Response.Status.OK.getStatusCode());
+
+//             assertThat(restContext.getHttpRequest().getMethod()).isEqualTo(HttpMethod.GET);
+//             assertThat(restContext.getHttpResponse().getStatusCode()).isEqualTo(Response.Status.OK.getStatusCode());
+//             assertThat(restContext.getHttpResponse().getContentType()).isEqualTo("application/json");
+//             List list = (List) restContext.getHttpResponse().getEntity();
+//             assertThat(list.size()).isEqualTo(1);
+             }
+          });
+    }
 }
